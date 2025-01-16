1d4fe78106e11e3856c42375517c5cb3fa275ac4
ShaimaaMK <128982071+ShaimaaMK@users.noreply.github.com>
2025-01-16 12:31:23 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "vm_name" {
+  description = "Nom de la VM Spot à recréer"
+  type        = string
+}
+
+variable "size" {
+  description = "Taille de la VM Spot "
+  type        = string
+}
+
+variable "region" {
+  description = "Région Azure "
+  type        = string
+}
+
+variable "os_disk_size_gb" {
+  description = "Taille du disque OS"
+  type        = number
+  default     = 64
+}
+
+variable "admin_username" {
+  description = "Nom d'utilisateur administrateur"
+  type        = string
+  default     = "shama"
+}
+
+variable "admin_password" {
+  description = "Mot de passe administrateur"
+  type        = string
+  sensitive   = true
+}
