2e40067a1a97375de6323979125abaf9325aa14a
ShaimaaMK <128982071+ShaimaaMK@users.noreply.github.com>
2025-01-17 10:42:20 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,12 +4,12 @@ variable "vm_name" {
 }
 
 variable "size" {
-  description = "Taille de la VM Spot "
+  description = "Taille de la VM Spot"
   type        = string
 }
 
 variable "region" {
-  description = "Région Azure "
+  description = "Région Azure"
   type        = string
 }
 
@@ -17,6 +17,12 @@ variable "os_disk_size" {
   description = "Taille du disque OS"
   type        = number
   default     = 64
+}
+
+variable "resource_group" {
+  description = "Nom du groupe de ressources"
+  type        = string
+  default     = "rg-spot-vms" 
 }
 
 variable "admin_username" {
@@ -28,5 +34,29 @@ variable "admin_username" {
 variable "admin_password" {
   description = "Mot de passe administrateur"
   type        = string
-  default   = "Str0ngP@ssw0rd!"
+  default     = "Str0ngP@ssw0rd!"
+}
+
+variable "image_publisher" {
+  description = "Publisher de l'image de la VM"
+  type        = string
+  default     = "Canonical"  
+}
+
+variable "image_offer" {
+  description = "Offer de l'image de la VM"
+  type        = string
+  default     = "0001-com-ubuntu-confidential-vm-focal"  
+}
+
+variable "image_sku" {
+  description = "SKU de l'image de la VM"
+  type        = string
+  default     = "20_04-lts-cvm"
+}
+
+variable "image_version" {
+  description = "Version de l'image de la VM"
+  type        = string
+  default     = "20.04.202111100"
 }
