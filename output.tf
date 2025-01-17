0b28a77a62a0ef3a48d4a850ae735a08403d2e73
ShaimaaMK <128982071+ShaimaaMK@users.noreply.github.com>
2025-01-17 10:38:10 +0100

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,8 @@
+output "resource_group" {
+  description = "Nom du groupe de ressources utilisé"
+  value       = azurerm_resource_group.rg.name
+}
+
 output "public_ip_address" {
   description = "Adresse IP publique de la VM"
   value       = azurerm_public_ip.pip.ip_address
@@ -10,5 +15,5 @@ output "admin_username" {
 
 output "vm_id" {
   description = "ID de la VM Spot"
-  value       = azurerm_linux_virtual_machine.spot_vm.id
+  value       = azurerm_linux_virtual_machine.vm_spot.id
 }
