6afc591f5841ba06405bda24bacaa882b361328b
ShaimaaMK <128982071+ShaimaaMK@users.noreply.github.com>
2025-01-24 16:39:06 +0100

    Create variables.tf

---
+++ modules/shared/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group" {
+  description = "Nom du groupe de ressources Azure"
+  type        = string
+}
+
+variable "region" {
+  description = "Région Azure"
+  type        = string
+}
