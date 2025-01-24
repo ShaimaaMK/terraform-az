bb2cbfb7b59336325040837fbd43cd59fcb46437
ShaimaaMK <128982071+ShaimaaMK@users.noreply.github.com>
2025-01-24 17:07:15 +0100

    Create output.tf

---
+++ modules/vm-spot/output.tf
@@ -0,0 +1,14 @@
+output "vm_id" {
+  description = "ID de la VM Spot recréée"
+  value       = azurerm_linux_virtual_machine.spot_vm.id
+}
+
+output "snapshot_id" {
+  description = "ID du snapshot utilisé"
+  value       = azurerm_managed_disk.os_disk_from_snapshot.id
+}
+
+output "public_ip" {
+  description = "Adresse IP publique de la VM recréée"
+  value       = azurerm_public_ip.pip.ip_address
+}
