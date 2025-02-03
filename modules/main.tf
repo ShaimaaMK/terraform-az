e1636fc81b141ac0bf8ec2b1b6aa46860f146f92
ShaimaaMK <128982071+ShaimaaMK@users.noreply.github.com>
2025-02-03 14:01:40 +0100

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -4,10 +4,7 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "=3.64.0"
     }
-    azapi = {
-      source  = "azure/azapi"
-      version = "~> 1.5.0"  # Or whatever recent version you'd like
-    }
+    
   }
 }
 
