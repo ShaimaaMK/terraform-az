a13644a11202cbc65ef450ef6aa8e32890bbe3b3
ShaimaaMK <128982071+ShaimaaMK@users.noreply.github.com>
2025-02-06 17:32:30 +0100

    Update main.tf

--- modules/vm-spot/main.tf
+++ modules/vm-spot/main.tf
@@ -33,101 +33,97 @@ resource "azurerm_network_interface" "nic" {
 }
 
 resource "azurerm_managed_disk" "os_disk_from_snapshot" {
-  name                    = "${var.vm_name}-osdisk"
-  location                = var.region
-  resource_group_name     = var.resource_group
-  storage_account_type    = "Standard_LRS"
-  create_option           = "Copy"
-  source_resource_id      = var.snapshot_id
-  disk_size_gb            = var.os_disk_size
-}
-
-resource "azurerm_virtual_machine" "spot_vm" {
-  name                = "${var.vm_name}-spot"
+  name                = "${var.vm_name}-spot-osdisk"
   location            = var.region
   resource_group_name = var.resource_group
-
-  network_interface_ids = [azurerm_network_interface.nic.id]
-  vm_size               = var.size
-
-  # Bloc storage_os_disk pour spécifier le disque attaché
-  storage_os_disk {
-    name            = "${var.vm_name}-osdisk"
-    managed_disk_id = azurerm_managed_disk.os_disk_from_snapshot.id  # Référence au disque géré
-    create_option   = "Attach"  # Indique que nous attachons un disque existant
-    caching         = "ReadWrite"
-    os_type         = "Linux"  # Type du système d'exploitation
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  tags = merge(
-    {
-      managed_by = "terraform"
-    },
-    var.tags
-  )
-
-  lifecycle {
-    create_before_destroy = true
-  }
+  storage_account_type = "Standard_LRS"
+  create_option       = "Copy"
+  source_resource_id  = var.snapshot_id
+  disk_size_gb        = var.os_disk_size
 }
 
-# Utilisation de null_resource pour mettre à jour la VM et appliquer les configurations Spot et Trusted Launch
-resource "null_resource" "update_vm_security_and_attach_disk" {
-  provisioner "local-exec" {
-    command = <<EOT
-      # Mettre à jour la VM pour la rendre Spot
-      az vm update \
-        --resource-group ${var.resource_group} \
-        --name ${azurerm_virtual_machine.spot_vm.name} \
-        --set priority=Spot \
-        --set evictionPolicy=Deallocate  # Utiliser Deallocate pour la politique d'éviction
 
-      # Appliquer le type de sécurité Trusted Launch
-      az vm update \
-        --resource-group ${var.resource_group} \
-        --name ${azurerm_virtual_machine.spot_vm.name} \
-        --set securityProfile.securityType="TrustedLaunch"
 
-      # Attacher le disque après mise à jour
-      az vm disk attach \
-        --resource-group ${var.resource_group} \
-        --vm-name ${azurerm_virtual_machine.spot_vm.name} \
-        --name ${azurerm_managed_disk.os_disk_from_snapshot.name}
-    EOT
-  }
+resource "azapi_resource" "spot_vm" {
+  type      = "Microsoft.Compute/virtualMachines@2022-03-01"  
+  
+  name      = "${var.vm_name}-spot"
+  parent_id = azurerm_resource_group.this.id  
+  location  = var.region
+  tags      = var.tags
+
+  body = jsonencode({
+    properties = {
+      hardwareProfile = {
+        vmSize = var.size
+      }
+      priority       = "Spot"
+      evictionPolicy = "Delete"  
+
+      securityProfile = {
+        securityType = "TrustedLaunch"
+      }
+
+      networkProfile = {
+        networkInterfaces = [
+          {
+            id = azurerm_network_interface.nic.id
+          }
+        ]
+      }
+
+      storageProfile = {
+        osDisk = {
+          name         = "${var.vm_name}-osdisk"
+          createOption = "Attach"
+          caching      = "ReadWrite"
+          osType       = "Linux"
+          managedDisk  = {
+            id = azurerm_managed_disk.os_disk_from_snapshot.id
+          }
+        }
+      }
+
+      osProfile = {
+        computerName         = "${var.vm_name}-spot"
+        adminUsername        = var.admin_username
+        adminPassword        = var.admin_password
+        linuxConfiguration = {
+          disablePasswordAuthentication = false
+        }
+      }
+    }
+
+    identity = {
+      type = "SystemAssigned"
+    }
+  })
 
   depends_on = [
-    azurerm_virtual_machine.spot_vm,
+    azurerm_network_interface.nic,
     azurerm_managed_disk.os_disk_from_snapshot
   ]
 }
+resource "azapi_resource" "custom_script_extension" {
+  type      = "Microsoft.Compute/virtualMachines/extensions@2022-03-01"
+  name      = "customScript"
+  parent_id = azapi_resource.spot_vm.id 
+  location  = var.region
 
-resource "azurerm_virtual_machine_extension" "custom_script" {
-  name                 = "${azurerm_virtual_machine.spot_vm.name}-cse"
-  virtual_machine_id   = azurerm_virtual_machine.spot_vm.id
-  publisher            = "Microsoft.Azure.Extensions"
-  type                 = "CustomScript"
-  type_handler_version = "2.1"
+  body = jsonencode({
+    properties = {
+      publisher            = "Microsoft.Azure.Extensions"
+      type                 = "CustomScript"
+      typeHandlerVersion   = "2.1"
+      autoUpgradeMinorVersion = true
 
-  settings = jsonencode({
-    fileUris  = ["https://raw.githubusercontent.com/ShaimaaMK/terraform-az/main/modules/vm-spot/cloud-init.yaml"]
-    commandToExecute = "sh cloud-init.yaml"
+      settings = {
+        fileUris          = ["https://raw.githubusercontent.com/ShaimaaMK/terraform-az/main/modules/vm-spot/cloud-init.yaml"]
+        commandToExecute  = "sh cloud-init.yaml"
+      }
+    }
   })
-  
-  tags = merge(
-    {
-      managed_by = "terraform"
-    },
-    var.tags
-  )
+
+  depends_on = [azapi_resource.spot_vm]
 }
 
-resource "azurerm_role_assignment" "snapshot_creator" {
-  scope                = var.resource_group_id
-  role_definition_name = "Contributor"
-  principal_id         = azurerm_virtual_machine.spot_vm.identity[0].principal_id
-}
